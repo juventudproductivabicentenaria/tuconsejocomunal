27e15ddab497f6e26213e0b0ec744513e852fd93
EstudianteGit <estudiantegit@gmail.com>
2016-09-19 08:27:46 -0430

    Se creo la estructura del módulo de gestión de los consejos comunales con el comando de odoo scaffold

---
+++ tcc_gentionconsejos/models.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+
+from openerp import models, fields, api
+
+# class tcc_gentionconsejos(models.Model):
+#     _name = 'tcc_gentionconsejos.tcc_gentionconsejos'
+
+#     name = fields.Char()
