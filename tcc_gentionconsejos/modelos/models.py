08ac4392f17febb5c1f68f975d86440abaf0b298
Estudiantegit <estudiantegit@gmail.com>
2016-10-05 18:16:26 -0400

    elimine el módulo  tcc_gestion...

--- tcc_gentionconsejos/modelos/models.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from openerp import models, fields, api
-
-# class tcc_gentionconsejos(models.Model):
-#     _name = 'tcc_gentionconsejos.tcc_gentionconsejos'
-
-#     name = fields.Char()
