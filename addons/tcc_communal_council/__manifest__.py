c76cb93b1f056962003b18beee247d1f4acc8a4b
Jeison Pernía <jeisonestudio@gmail.com>
2017-07-18 19:37:36 -0400

    Agregar Método de encuesta por defecto
    	Se realiza un método que crea una encuesta por defecto
    al crearse un COnsejo COmunal (encuesta Participación Comunitara)
    	Se agregan Botón de encuestar y de enviar encuesta en personas
    Encuestar: envía al formulario de ralizar la encuesta con la
    ayuda del consejo comunal.
    Enviar encuesta: levanta un wizard que envía una invitación
    por correo electronico al usuario

--- addons/tcc_communal_council/__manifest__.py
+++ addons/tcc_communal_council/__manifest__.py
@@ -33,7 +33,6 @@ Cada Consejo comunal podrá realizar registros de noticias, tareas y encuentas y
         'data/ir_sequence_data.xml',                # Sequencia codigo de familia
         'data/family_data.xml',                     # Data de familia
         'data/persons_data.xml',                    # Data de Personas
-        'data/survey_default_data.xml',             # Data de Personas
         
         'security/tcc_group_consejo/ir.model.access.csv',
         'security/tcc_group_residente/ir.model.access.csv',
